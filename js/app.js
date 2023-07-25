3404b4308075d434559f08c2554f409bf6cbe54e
jaydww <jamesdwalsh@hotmail.com>
2023-07-25 12:15:53 -0700

    set calorie limit

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ class CalorieTracker {
 		this._render();
 	}
 
+	setLimit(calorieLimit) {
+		this._calorieLimit = calorieLimit;
+		this._displayCaloriesLimit();
+		this._render();
+	}
+
 	// Private methods //
 
 	_displayCaloriesTotal() {
@@ -229,6 +235,10 @@ class App {
 		document
 			.getElementById('reset')
 			.addEventListener('click', this._reset.bind(this));
+
+		document
+			.getElementById('limit-form')
+			.addEventListener('submit', this._setLimit.bind(this));
 	}
 
 	_newItem(type, e) {
@@ -296,6 +306,24 @@ class App {
 		document.getElementById('workout-items').innerHTML = '';
 		document.getElementById('filter-meals').value = '';
 		document.getElementById('filter-workouts').value = '';
+	}
+
+	_setLimit(e) {
+		e.preventDefault();
+
+		const limit = document.getElementById('limit');
+
+		if (limit.value === '') {
+			alert('Please add a limit');
+			return;
+		}
+
+		this._tracker.setLimit(+limit.value);
+		limit.value = '';
+
+		const modalEl = document.getElementById('limit-modal');
+		const modal = bootstrap.Modal.getInstance(modalEl);
+		modal.hide();
 	}
 }
 
